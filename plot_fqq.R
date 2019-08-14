4983bc8b28a5b1b9cf7b1be202f2dd058caeabd2
vforget <forgetta@gmail.com>
2019-08-14 11:25:55 -0400

    Handle missing command-line arguments

--- plot_fqq.R
+++ plot_fqq.R
@@ -8,7 +8,16 @@ p.f = args[1]
 p.m = as.numeric(args[2])
 p.e = as.numeric(args[3])
 
-qq.f = paste(p.f, ".qq.png", sep="")
+if (is.na(p.m)){
+   p.m = 0.001
+}
+if (is.na(p.e)){
+   p.e = 0.001
+}
+
+message(paste0("p.m = ",p.m,"\np.e = ",p.e)) 
+qq.f = paste0(p.f, ".qq.png")
+message(paste0("output file name = ", qq.f))
 
 message("Loading data >> ", appendLF=FALSE)
 DT <- fread(p.f, header=TRUE)
